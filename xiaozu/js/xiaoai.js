7c392c7122c9c09fd659f758d190339965cbca2e
刘萧 <2861218857@qq.com>
2019-08-14 21:15:31 +0800

    19/8/14/xiaoai

---
+++ xiaozu/js/xiaoai.js
@@ -0,0 +1,17 @@
+// top区域
+	var oBtn1=document.getElementById('btn1')
+	var oBtn2=document.getElementById('btn2')
+	var oBtn3=document.getElementById('btn3')
+	var oBtn4=document.getElementById('btn4')
+	var oBox1=document.getElementById('box1')
+	var oBox2=document.getElementById('box2')
+	var oBox3=document.getElementById('box3')
+	var oBox4=document.getElementById('box4')
+	oBtn.onclick= function(){
+		if (oBox.style.display == 'none') {
+			oBox.style.display = 'block';
+		}else{
+			oBox.style.display = 'none';
+		}
+	}
+//header区域
